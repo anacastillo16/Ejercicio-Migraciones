8674d5de45f763a4d464e87f964cf1faa2dc90be
Ana Castillo Ramírez <anacastilloramirez16@gmail.com>
2025-02-17 19:08:10 +0100

    Haciendo ejercicio

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Inmobiliaria Ana</h1>
+        <a href="{{ route('propiedades.create') }}">Crear Propiedad</a>
+        <a href="{{ route('agentes.create') }}">Crear Agente</a>
+        <a href="{{ route('categorias.create') }}">Crear Categoría</a>
+    </header>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AgentesController;
+use App\Http\Controllers\CategoriasController;
+use App\Http\Controllers\PropiedadesController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('agentes', AgentesController::class);
+Route::resource('categorias', CategoriasController::class);
+Route::resource('propiedades', PropiedadesController::class);
